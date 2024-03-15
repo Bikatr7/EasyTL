26fb16c6f234b994991a800da4c2aa792a3a2d9c
Bikatr7 <Tetralon07@gmail.com>
2024-03-15 05:27:16 -0600

    added deepl translate

--- src/easytl/deepl_service.py
+++ src/easytl/deepl_service.py
@@ -24,6 +24,54 @@ class DeepLService:
     splitting_tags:str | typing.List[str] | None = None
     ignore_tags:str | typing.List[str] | None = None
 
+##-------------------start-of-set_attributes()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    @staticmethod
+    def set_attributes(target_lang:str | Language = "EN",
+                        source_lang:str | Language | None = None,
+                        context:str | None = None,
+                        split_sentences:typing.Literal["OFF", "ALL", "NO_NEWLINES"] |  SplitSentences | None = "ALL",
+                        preserve_formatting:bool | None = None,
+                        formality:typing.Literal["default", "more", "less", "prefer_more", "prefer_less"] | Formality | None = None,
+                        glossary:str | GlossaryInfo | None = None,
+                        tag_handling:typing.Literal["xml", "html"] | None = None,
+                        outline_detection:bool | None = None,
+                        non_splitting_tags:str | typing.List[str] | None = None,
+                        splitting_tags:str | typing.List[str] | None = None,
+                        ignore_tags:str | typing.List[str] | None = None) -> None:
+
+        """
+
+        Sets the attributes of the DeepL client.
+
+        Parameters:
+        target_lang (string or Language) : The target language to translate to.
+        source_lang (string or Language or None) : The source language to translate from.
+        context (string or None) : The context of the text.
+        split_sentences (literal or SplitSentences or None) : The split sentences option.
+        preserve_formatting (bool or None) : The preserve formatting option.
+        formality (literal or Formality or None) : The formality option.
+        glossary (string or GlossaryInfo or None) : The glossary option.
+        tag_handling (literal or None) : The tag handling option.
+        outline_detection (bool or None) : The outline detection option.
+        non_splitting_tags (string or list - str or None) : The non-splitting tags option.
+        splitting_tags (string or list - str or None) : The splitting tags option.
+        ignore_tags (string or list - str or None) : The ignore tags option.
+
+        """
+
+        DeepLService.target_lang = target_lang
+        DeepLService.source_lang = source_lang
+        DeepLService.context = context
+        DeepLService.split_sentences = split_sentences
+        DeepLService.preserve_formatting = preserve_formatting
+        DeepLService.formality = formality
+        DeepLService.glossary = glossary
+        DeepLService.tag_handling = tag_handling
+        DeepLService.outline_detection = outline_detection
+        DeepLService.non_splitting_tags = non_splitting_tags
+        DeepLService.splitting_tags = splitting_tags
+        DeepLService.ignore_tags = ignore_tags
 
 ##-------------------start-of-translate()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
--- src/easytl/easytl.py
+++ src/easytl/easytl.py
@@ -1,12 +1,15 @@
 ## built-in libraries
 import typing
 
+## third-party libraries
+from deepl.api_data import Language, SplitSentences, Formality, GlossaryInfo, TextResult
+
 ## custom modules
 from easytl.deepl_service import DeepLService
 from easytl.gemini_service import GeminiService
 from easytl.openai_service import OpenAIService
 
-from exceptions import DeepLException, 
+from exceptions import DeepLException
 
 ##-------------------start-of-set_api_key()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -62,3 +65,62 @@ def test_api_key_validity(api_type:typing.Literal["deepl", "gemini", "openai"]) 
     
     ## need to add the other services here
     ## but before that, need to convert said services to have non-asynchronous methods, as to not force the user to use async/await
+
+##-------------------start-of-deepl_translate()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def deepl_translate(text:typing.Union[str, typing.Iterable],
+                    target_lang:str | Language = "EN",
+                    override_previous_settings:bool = True,
+                    source_lang:str | Language | None = None,
+                    context:str | None = None,
+                    split_sentences:typing.Literal["OFF", "ALL", "NO_NEWLINES"] |  SplitSentences | None = "ALL",
+                    preserve_formatting:bool | None = None,
+                    formality:typing.Literal["default", "more", "less", "prefer_more", "prefer_less"] | Formality | None = None,
+                    glossary:str | GlossaryInfo | None = None,
+                    tag_handling:typing.Literal["xml", "html"] | None = None,
+                    outline_detection:bool | None = None,
+                    non_splitting_tags:str | typing.List[str] | None = None,
+                    splitting_tags:str | typing.List[str] | None = None,
+                    ignore_tags:str | typing.List[str] | None = None) -> typing.Union[typing.List[str], str]:
+    
+    """
+
+    Translates the given text to the target language using DeepL.
+
+    This function assumes that the API key has already been set.
+
+    Parameters:
+    text (string or iterable) : The text to translate.
+    target_lang (string or Language) : The target language to translate to.
+    override_previous_settings (bool) : Whether to override the previous settings that were used during the last call to this function.
+    source_lang (string or Language or None) : The source language to translate from.
+    context (string or None) : The context of the text.
+    split_sentences (literal or SplitSentences or None) : The split sentences option.
+    preserve_formatting (bool or None) : The preserve formatting option.
+    formality (literal or Formality or None) : The formality option.
+    glossary (string or GlossaryInfo or None) : The glossary option.
+    tag_handling (literal or None) : The tag handling option.
+    outline_detection (bool or None) : The outline detection option.
+    non_splitting_tags (string or list - str or None) : The non-splitting tags option.
+    splitting_tags (string or list - str or None) : The splitting tags option.
+    ignore_tags (string or list - str or None) : The ignore tags option.
+
+    Returns:
+    translation (TextResult or list - TextResult) : The translation result.
+
+    """
+
+    if(override_previous_settings == True):
+        DeepLService.set_attributes(target_lang, 
+                                    source_lang, 
+                                    context, 
+                                    split_sentences,
+                                    preserve_formatting, 
+                                    formality, 
+                                    glossary, tag_handling, outline_detection, non_splitting_tags, splitting_tags, ignore_tags)
+        
+    if(isinstance(text, str)):
+        return DeepLService.translate(text).text # type: ignore
+    
+    else:
+        return [DeepLService.translate(t).text for t in text] # type: ignore
