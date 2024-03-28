377a766e6997d0e186d425f7120e4b1eba238401
Bikatr7 <Tetralon07@gmail.com>
2024-03-27 23:20:54 -0600

    refactored gemini service to have sync methods, also redesigned approach to be focused on a default translation instruction, also made it clearer which ones are internal

--- src/easytl/gemini_service.py
+++ src/easytl/gemini_service.py
@@ -7,24 +7,25 @@ import google.generativeai as genai
 
 class GeminiService:
 
-    default_translation_instructions:str = "Please translate the following text into English."
+    _default_translation_instructions:str = "Please translate the following text into English."
 
-    model:str = "gemini-pro"
-    prompt:str = ""
-    temperature:float = 0.5
-    top_p:float = 0.9
-    top_k:int = 40
-    candidate_count:int = 1
-    stream:bool = False
-    stop_sequences:typing.List[str] | None = None
-    max_output_tokens:int | None = None
+    _model:str = "gemini-pro"
+    _text_to_translate:str = ""
+    _temperature:float = 0.5
+    _top_p:float = 0.9
+    _top_k:int = 40
+    _candidate_count:int = 1
+    _stream:bool = False
+    _stop_sequences:typing.List[str] | None = None
+    _max_output_tokens:int | None = None
 
-    client:genai.GenerativeModel
-    generation_config:GenerationConfig
+    _client:genai.GenerativeModel
+    _generation_config:GenerationConfig
 
-    decorator_to_use:typing.Union[typing.Callable, None] = None
+    _decorator_to_use:typing.Union[typing.Callable, None] = None
 
-    safety_settings = [
+    ## I don't plan to allow users to change these settings, as I believe that translations should be as accurate as possible, avoiding any censorship or filtering of content.
+    _safety_settings = [
         {
             "category": "HARM_CATEGORY_DANGEROUS",
             "threshold": "BLOCK_NONE",
@@ -47,26 +48,26 @@ class GeminiService:
         },
     ]
 
-##-------------------start-of-set_api_key()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+##-------------------start-of-_set_api_key()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
-    def set_api_key(api_key:str) -> None:
+    def _set_api_key(api_key:str) -> None:
 
         """
 
         Sets the API key for the Gemini client.
 
         Parameters:
-        api_key (string) : The API key to set.
+        api_key (string) : The API key.
 
         """
 
         genai.configure(api_key=api_key)
 
-##-------------------start-of-set_decorator()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+##-------------------start-of-_set_decorator()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
-    def set_decorator(decorator:typing.Callable) -> None:
+    def _set_decorator(decorator:typing.Callable) -> None:
 
         """
 
@@ -77,12 +78,12 @@ class GeminiService:
 
         """
 
-        GeminiService.decorator_to_use = decorator
+        GeminiService._decorator_to_use = decorator
 
-##-------------------start-of-redefine_client()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+##-------------------start-of-_redefine_client()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
-    def redefine_client() -> None:
+    def _redefine_client() -> None:
 
         """
 
@@ -90,149 +91,179 @@ class GeminiService:
 
         """
 
-        GeminiService.client = genai.GenerativeModel(GeminiService.model,
-                                                     safety_settings=GeminiService.safety_settings)
+        GeminiService._client = genai.GenerativeModel(model_name=GeminiService._model,
+                                                     safety_settings=GeminiService._safety_settings)
 
-        GeminiService.generation_config = GenerationConfig(candidate_count=GeminiService.candidate_count,
-                                                           max_output_tokens=GeminiService.max_output_tokens,
-                                                           stop_sequences=GeminiService.stop_sequences,
-                                                            temperature=GeminiService.temperature,
-                                                            top_p=GeminiService.top_p,
-                                                            top_k=GeminiService.top_k)
+        GeminiService._generation_config = GenerationConfig(candidate_count=GeminiService._candidate_count,
+                                                           max_output_tokens=GeminiService._max_output_tokens,
+                                                           stop_sequences=GeminiService._stop_sequences,
+                                                            temperature=GeminiService._temperature,
+                                                            top_p=GeminiService._top_p,
+                                                            top_k=GeminiService._top_k)
 
-##-------------------start-of-redefine_client_decorator()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+##-------------------start-of-_redefine_client_decorator()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
-    def redefine_client_decorator(func):
+    def _redefine_client_decorator(func):
+
+        """
+
+        Wraps a function to redefine the Gemini client before doing anything that requires the client.
+
+        Parameters:
+        func (callable) : The function to wrap.
+
+        Returns:
+        wrapper (callable) : The wrapped function.
+
+        """
+
         def wrapper(*args, **kwargs):
-            GeminiService.redefine_client() 
+            GeminiService._redefine_client() 
             return func(*args, **kwargs)
+        
         return wrapper
             
 ##-------------------start-of-count_tokens()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
-    def count_tokens(prompt:str) -> int:
+    def count_tokens(text:str) -> int:
 
         """
 
-        Counts the number of tokens in a prompt.
+        Counts the number of tokens in the given text.
 
         Parameters:
-        prompt (string) : The prompt to count the tokens of.
+        text (string) : The text to count tokens in.
 
         Returns:
-        count (int) : The number of tokens in the prompt.
+        total_tokens (int) : The number of tokens in the text.
 
         """
 
-        return genai.GenerativeModel(GeminiService.model).count_tokens(prompt).total_tokens
+        return genai.GenerativeModel(GeminiService._model).count_tokens(text).total_tokens
 
-##-------------------start-of-_translate_message_async()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+##-------------------start-of-_translate_text_async()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
-    @redefine_client_decorator
-    async def _translate_message_async(translation_instructions:str, translation_prompt:str) -> str:
+    @_redefine_client_decorator
+    async def _translate_text_async(text_to_translate:str, translation_instructions:typing.Optional[str]) -> str:
 
         """
 
-        Translates a prompt.
+        Asynchronously translates a text.
+        Instructions default to translating whatever text is input into English.
 
         Parameters:
-        translation_prompt (object - ModelTranslationMessage) : The prompt to translate.
+        text_to_translate (string) : The text to translate.
+        translation_instructions (string) : The instructions for the translation.
 
         Returns:
         output (string) : The translation.
 
         """
 
-        if(GeminiService.decorator_to_use is None):
-            return await GeminiService.__translate_message_async(translation_instructions, translation_prompt)
+        if(translation_instructions is None):
+            translation_instructions = GeminiService._default_translation_instructions
 
-        decorated_function = GeminiService.decorator_to_use(GeminiService.__translate_message_async)
-        return await decorated_function(translation_instructions, translation_prompt)
+        if(GeminiService._decorator_to_use is None):
+            return await GeminiService.__translate_text_async(translation_instructions, text_to_translate)
+
+        decorated_function = GeminiService._decorator_to_use(GeminiService.__translate_text_async)
+        return await decorated_function(translation_instructions, text_to_translate)
     
 ##-------------------start-of-_translate_message()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
     @staticmethod
-    @redefine_client_decorator
-    def _translate_message(translation_instructions:str, translation_prompt:str) -> str:
-    
+    @_redefine_client_decorator
+    def _translate_message(text_to_translate:str, translation_instructions:typing.Optional[str]) -> str:
+
         """
 
-        Translates a prompt.
+        Synchronously translates a text.
+        Instructions default to translating whatever text is input into English.
 
         Parameters:
-        translation_prompt (object - ModelTranslationMessage) : The prompt to translate.
+        text_to_translate (string) : The text to translate.
+        translation_instructions (string) : The instructions for the translation.
 
         Returns:
         output (string) : The translation.
 
         """
 
-        if(GeminiService.decorator_to_use is None):
-            return GeminiService.__translate_message(translation_instructions, translation_prompt)
+        if(translation_instructions is None):
+            translation_instructions = GeminiService._default_translation_instructions
 
-        decorated_function = GeminiService.decorator_to_use(GeminiService.__translate_message)
-        return decorated_function(translation_instructions, translation_prompt)
+        if(GeminiService._decorator_to_use is None):
+            return GeminiService.__translate_text(translation_instructions, text_to_translate)
+
+        decorated_function = GeminiService._decorator_to_use(GeminiService.__translate_text)
+        return decorated_function(translation_instructions, text_to_translate)
     
-##-------------------start-of-__translate_message()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+##-------------------start-of-__translate_text()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
     @staticmethod
-    def __translate_message(translation_instructions:str, translation_prompt:str) -> str:
+    def __translate_text(translation_instructions:str, text_to_translate:str) -> str:
 
         """
 
-        Translates a prompt.
+        Synchronously translates text.
 
         Parameters:
-        translation_prompt (string) : The prompt to translate.
+        translation_instructions (string) : The instructions for the translation.
+        text_to_translate (string) : The text to translate.
 
         Returns:
-        output (string) a string that gpt gives to us also known as the translation.
+        output (string) : The translation.
 
         """
 
-        response = GeminiService.client.generate_content(
-            contents=translation_instructions + "\n" + translation_prompt,
-            generation_config=GeminiService.generation_config,
-            safety_settings=GeminiService.safety_settings,
-            stream=GeminiService.stream
+        _response = GeminiService._client.generate_content(
+            contents=translation_instructions + "\n" + text_to_translate,
+            generation_config=GeminiService._generation_config,
+            safety_settings=GeminiService._safety_settings,
+            stream=GeminiService._stream
         )
+        
+        ## may need to add some error handling here later for if the response is not successful
 
-        return response.text
+        return _response.text
 
 ##-------------------start-of-__translate_message_async()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
-    async def __translate_message_async(translation_instructions:str, translation_prompt:str) -> str:
+    async def __translate_text_async(translation_instructions:str, text_to_translate:str) -> str:
 
         """
 
-        Translates a prompt.
+        Asynchronously translates text.
 
         Parameters:
-        translation_prompt (string) : The prompt to translate.
+        translation_instructions (string) : The instructions for the translation.
+        text_to_translate (string) : The text to translate.
 
         Returns:
-        output (string) a string that gpt gives to us also known as the translation.
+        output (string) : The translation.
 
         """
 
-        response = await GeminiService.client.generate_content_async(
-            contents=translation_instructions + "\n" + translation_prompt,
-            generation_config=GeminiService.generation_config,
-            safety_settings=GeminiService.safety_settings,
-            stream=GeminiService.stream
+        _response = await GeminiService._client.generate_content_async(
+            contents=translation_instructions + "\n" + text_to_translate,
+            generation_config=GeminiService._generation_config,
+            safety_settings=GeminiService._safety_settings,
+            stream=GeminiService._stream
         )
+        
+        ## may need to add some error handling here later for if the response is not successful
 
-        return response.text
+        return _response.text
     
 ##-------------------start-of-test_api_key_validity()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
     @staticmethod
-    @redefine_client_decorator
-    async def test_api_key_validity() -> typing.Tuple[bool, typing.Union[Exception, None]]:
+    @_redefine_client_decorator
+    def _test_api_key_validity() -> typing.Tuple[bool, typing.Union[Exception, None]]:
 
         """
 
@@ -244,29 +275,29 @@ class GeminiService:
 
         """
 
-        validity = False
+        _validity = False
 
         try:
 
-            generation_config = GenerationConfig(candidate_count=1, max_output_tokens=1)
+            _generation_config = GenerationConfig(candidate_count=1, max_output_tokens=1)
 
-            await GeminiService.client.generate_content_async(
-                "Respond to this prompt with 1",generation_config=generation_config
+            GeminiService._client.generate_content(
+                "Respond to this with 1",generation_config=_generation_config
 
             )
 
-            validity = True
+            _validity = True
 
-            return validity, None
+            return _validity, None
 
-        except Exception as e:
+        except Exception as _e:
 
-            return validity, e
+            return _validity, _e
         
 ##-------------------start-of-get_decorator()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
-    def get_decorator() -> typing.Union[typing.Callable, None]:
+    def _get_decorator() -> typing.Union[typing.Callable, None]:
 
         """
 
@@ -277,4 +308,4 @@ class GeminiService:
 
         """
 
-        return GeminiService.decorator_to_use
+        return GeminiService._decorator_to_use
