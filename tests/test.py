67db2ea9c529ebf8b5cb881ab7d25b4543ed8363
Bikatr7 <Tetralon07@gmail.com>
2024-04-18 03:12:32 -0600

    updated test file

--- tests/test.py
+++ tests/test.py
@@ -17,17 +17,24 @@ async def main():
     EasyTL.set_api_key("gemini", gemini_api_key)
     EasyTL.set_api_key("openai", openai_api_key)
 
+    logging_directory = "C:/Users/Tetra/Desktop/"
 
-    print(EasyTL.deepl_translate("Hello, world!", target_lang="DE", logging_directory="C:/Users/Tetra/Desktop/"))
-    print(await EasyTL.deepl_translate_async("Hello, world!", target_lang="DE", logging_directory="C:/Users/Tetra/Desktop/"))
+    print("Deepl ------------------------------------------------")
+
+    print("Text response")
+
+    print(EasyTL.deepl_translate("Hello, world!", target_lang="DE", logging_directory=logging_directory))
+    print(await EasyTL.deepl_translate_async("Hello, world!", target_lang="DE", logging_directory=logging_directory))
     
-    print(EasyTL.deepl_translate("Hello, world!", target_lang="DE", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/").text) # type: ignore
-    result = await EasyTL.deepl_translate_async("Hello, world!", target_lang="DE", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/")
+    print(EasyTL.deepl_translate("Hello, world!", target_lang="DE", response_type="raw", logging_directory=logging_directory).text) # type: ignore
+    result = await EasyTL.deepl_translate_async("Hello, world!", target_lang="DE", response_type="raw", logging_directory=logging_directory)
 
     print(result.text) # type: ignore
 
-    results = EasyTL.deepl_translate(text=["Hello, world!", "Goodbye, world!"], target_lang="DE", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/")
-    async_results = await EasyTL.deepl_translate_async(text=["Hello, world!", "Goodbye, world!"], target_lang="DE", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/")
+    print("Raw response")
+
+    results = EasyTL.deepl_translate(text=["Hello, world!", "Goodbye, world!"], target_lang="DE", response_type="raw", logging_directory=logging_directory)
+    async_results = await EasyTL.deepl_translate_async(text=["Hello, world!", "Goodbye, world!"], target_lang="DE", response_type="raw", logging_directory=logging_directory)
 
     for result in results: # type: ignore
         print(result.text) # type: ignore
@@ -35,16 +42,22 @@ async def main():
     for result in async_results: # type: ignore
         print(result.text)
 
-    print(EasyTL.gemini_translate("Hello, world!", translation_instructions="Translate this to German.", logging_directory="C:/Users/Tetra/Desktop/"))
-    print(await EasyTL.gemini_translate_async("Hello, world!", translation_instructions="Translate this to German.", logging_directory="C:/Users/Tetra/Desktop/"))
+    print("Gemini ------------------------------------------------")
 
-    print(EasyTL.gemini_translate("Hello, world!", translation_instructions="Translate this to German.", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/").text) # type: ignore
-    result = await EasyTL.gemini_translate_async("Hello, world!", translation_instructions="Translate this to German.", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/")
+    print("Text response")
+
+    print(EasyTL.gemini_translate("Hello, world!", translation_instructions="Translate this to German.", logging_directory=logging_directory))
+    print(await EasyTL.gemini_translate_async("Hello, world!", translation_instructions="Translate this to German.", logging_directory=logging_directory))
+
+    print(EasyTL.gemini_translate("Hello, world!", translation_instructions="Translate this to German.", response_type="raw", logging_directory=logging_directory).text) # type: ignore
+    result = await EasyTL.gemini_translate_async("Hello, world!", translation_instructions="Translate this to German.", response_type="raw", logging_directory=logging_directory)
 
     print(result.text) # type: ignore
 
-    results = EasyTL.gemini_translate(text=["Hello, world!", "Goodbye, world!"], translation_instructions="Translate this to German.", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/")
-    async_results = await EasyTL.gemini_translate_async(text=["Hello, world!", "Goodbye, world!"], translation_instructions="Translate this to German.", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/")
+    print("Raw response")
+
+    results = EasyTL.gemini_translate(text=["Hello, world!", "Goodbye, world!"], translation_instructions="Translate this to German.", response_type="raw", logging_directory=logging_directory)
+    async_results = await EasyTL.gemini_translate_async(text=["Hello, world!", "Goodbye, world!"], translation_instructions="Translate this to German.", response_type="raw", logging_directory=logging_directory)
 
     for result in results: # type: ignore
         print(result.text) # type: ignore
@@ -52,17 +65,28 @@ async def main():
     for result in async_results: # type: ignore
         print(result.text)
 
+    print("JSON response")
 
-    print(EasyTL.openai_translate("Hello, world!", model="gpt-3.5-turbo", translation_instructions="Translate this to German.", logging_directory="C:/Users/Tetra/Desktop/"))
-    print(await EasyTL.openai_translate_async("Hello, world!", model="gpt-3.5-turbo", translation_instructions="Translate this to German.", logging_directory="C:/Users/Tetra/Desktop/"))
+    print(EasyTL.gemini_translate("Hello, world!", model="gemini-1.5-pro-latest", translation_instructions="Translate this to German. Format the response as JSON.", response_type="json", logging_directory=logging_directory))
+    print(await EasyTL.gemini_translate_async("Hello, world!", model="gemini-1.5-pro-latest", translation_instructions="Format the response as JSON parseable string. It should have 2 keys, one for input titled input, and one called output, which is the translation.", response_type="json", logging_directory=logging_directory))
+    
 
-    print(EasyTL.openai_translate("Hello, world!", model="gpt-3.5-turbo", translation_instructions="Translate this to German.", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/").choices[0].message.content) # type: ignore
-    result = await EasyTL.openai_translate_async("Hello, world!", model="gpt-3.5-turbo", translation_instructions="Translate this to German.", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/")
+    print("OpenAI ------------------------------------------------")
+
+    print("Text response")
+
+    print(EasyTL.openai_translate("Hello, world!", model="gpt-3.5-turbo", translation_instructions="Translate this to German.", logging_directory=logging_directory))
+    print(await EasyTL.openai_translate_async("Hello, world!", model="gpt-3.5-turbo", translation_instructions="Translate this to German.", logging_directory=logging_directory))
+
+    print(EasyTL.openai_translate("Hello, world!", model="gpt-3.5-turbo", translation_instructions="Translate this to German.", response_type="raw", logging_directory=logging_directory).choices[0].message.content) # type: ignore
+    result = await EasyTL.openai_translate_async("Hello, world!", model="gpt-3.5-turbo", translation_instructions="Translate this to German.", response_type="raw", logging_directory=logging_directory)
 
     print(result.choices[0].message.content) # type: ignore
 
-    results = EasyTL.openai_translate(text=["Hello, world!", "Goodbye, world!"], model="gpt-3.5-turbo", translation_instructions="Translate this to German.", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/")
-    async_results = await EasyTL.openai_translate_async(text=["Hello, world!", "Goodbye, world!"], model="gpt-3.5-turbo", translation_instructions="Translate this to German.", response_type="raw", logging_directory="C:/Users/Tetra/Desktop/")
+    print("Raw response")
+
+    results = EasyTL.openai_translate(text=["Hello, world!", "Goodbye, world!"], model="gpt-3.5-turbo", translation_instructions="Translate this to German.", response_type="raw", logging_directory=logging_directory)
+    async_results = await EasyTL.openai_translate_async(text=["Hello, world!", "Goodbye, world!"], model="gpt-3.5-turbo", translation_instructions="Translate this to German.", response_type="raw", logging_directory=logging_directory)
 
     for result in results: # type: ignore
         print(result.choices[0].message.content) # type: ignore
@@ -70,7 +94,10 @@ async def main():
     for result in async_results: # type: ignore
         print(result.choices[0].message.content ) # type: ignore
 
+    print("JSON response")
 
+    print(EasyTL.openai_translate("Hello, world!", model="gpt-3.5-turbo-0125", translation_instructions="Translate this to German. Format the response as JSON parseable string.", response_type="json", logging_directory=logging_directory))
+    print(await EasyTL.openai_translate_async("Hello, world!", model="gpt-3.5-turbo-0125", translation_instructions="Translate this to German. Format the response as JSON parseable string. It should have 2 keys, one for input titled input, and one called output, which is the translation.", response_type="json", logging_directory=logging_directory))
 
 if(__name__ == "__main__"):
     asyncio.run(main())
