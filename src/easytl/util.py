0d1a5c631ea0622ae2595ec19dd3906dadf796b3
Bikatr7 <Tetralon07@gmail.com>
2024-05-14 21:44:13 -0600

    update util.py to have updated cost info and models like flash 1.5, added valid_json_gemini_models

--- src/easytl/util.py
+++ src/easytl/util.py
@@ -421,8 +421,19 @@ def _estimate_cost(text:str | typing.Iterable, model:str, price_case:int | None 
             print("Warning: gemini-1.0-pro-vision-latest may change over time. Estimating cost assuming gemini-1.0-pro-vision-001 as it is the most recent version of gemini-1.0-pro-vision.")
             return _estimate_cost(text, model="gemini-1.0-pro-vision-001", price_case=8)
         
+        elif(model == "gemini-1.5-pro"):
+            return _estimate_cost(text, model=model, price_case=9)
+        
+        elif(model == "gemini-1.5-flash"):
+            return _estimate_cost(text, model=model, price_case=9)
+
         elif(model == "gemini-1.5-pro-latest"):
-            return _estimate_cost(text, model=model, price_case=8)
+            print("Warning: gemini-1.5-pro-latest may change over time. Estimating cost assuming gemini-1.5-pro as it is the most recent version of gemini-1.5-pro.")
+            return _estimate_cost(text, model="gemini-1.5-pro", price_case=9)
+        
+        elif(model == "gemini-1.5-flash-latest"):
+            print("Warning: gemini-1.5-flash-latest may change over time. Estimating cost assuming gemini-1.5-flash as it is the most recent version of gemini-1.5-flash.")
+            return _estimate_cost(text, model="gemini-1.5-flash", price_case=9)
         
   ##      elif(model == "gemini-1.0-ultra-latest"):
       ##      return _estimate_cost(text, model=model, price_case=8)
@@ -516,10 +527,20 @@ ALLOWED_GEMINI_MODELS = [
     "gemini-1.0-pro-vision",
     "gemini-1.0-pro-vision-latest",
     "gemini-1.5-pro-latest",
+    "gemini-1.5-pro",
+    "gemini-1.5-flash-latest",
+    "gemini-1.5-flash",
   ##  "gemini-1.0-ultra-latest",
     "gemini-pro",
     "gemini-pro-vision",
   ##  "gemini-ultra"
+]
+
+VALID_JSON_GEMINI_MODELS = [
+    "gemini-1.5-pro-latest",
+    "gemini-1.5-pro",
+    "gemini-1.5-flash-latest",
+    "gemini-1.5-flash",
 ]
 
 ALLOWED_ANTHROPIC_MODELS = [
@@ -568,6 +589,9 @@ MODEL_COSTS = {
     "gemini-pro": {"price_case": 9, "_input_cost": 0.0, "_output_cost": 0.0},
     "gemini-pro-vision": {"price_case": 9, "_input_cost": 0.0, "_output_cost": 0.0},
  ##   "gemini-ultra": {"price_case": 9, "_input_cost": 0.0, "_output_cost": 0.0}
+    "gemini-1.5-pro": {"price_case": 9, "_input_cost": 0.0, "_output_cost": 0.0},
+    "gemini-1.5-flash-latest": {"price_case": 9, "_input_cost": 0.0, "_output_cost": 0.0},
+    "gemini-1.5-flash": {"price_case": 9, "_input_cost": 0.0, "_output_cost": 0.0},
 
     ## grouping anthropic models together
     "claude-3-haiku-20240307": {"price_case": 11, "_input_cost": 0.00025, "_output_cost": 0.00125},
