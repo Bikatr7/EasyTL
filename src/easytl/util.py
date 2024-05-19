d8e7b57957332beabf7ec095cb326bf0f2776656
Bikatr7 <Tetralon07@gmail.com>
2024-05-19 15:11:03 -0600

    added MODEL_MAX_TOKENS constant

--- src/easytl/util.py
+++ src/easytl/util.py
@@ -711,3 +711,31 @@ MODEL_COSTS = {
     "claude-3-opus-20240229": {"price_case": 13, "_input_cost": 0.015, "_output_cost": 0.075}
 
 }
+
+MODEL_MAX_TOKENS = {
+
+    ## openai models
+    "gpt-4o-2024-05-13": {"max_input_tokens": 128000, "max_output_tokens": 4096},
+    "gpt-4-turbo-2024-04-09": {"max_input_tokens": 128000, "max_output_tokens": 4096},
+    "gpt-4-0125-preview": {"max_input_tokens": 128000, "max_output_tokens": 4096},
+    "gpt-4-1106-preview": {"max_input_tokens": 128000, "max_output_tokens": 4096},
+    "gpt-4-1106-vision-preview": {"max_input_tokens": 128000, "max_output_tokens": 4096},
+    "gpt-4-0613": {"max_input_tokens": 8192, "max_output_tokens": 4096},
+    "gpt-4-32k-0613": {"max_input_tokens": 32768, "max_output_tokens": 4096},
+    "gpt-3.5-turbo-0125": {"max_input_tokens": 16385, "max_output_tokens": 4096},
+    "gpt-3.5-turbo-1106": {"max_input_tokens": 16385, "max_output_tokens": 4096},
+    "gpt-3.5-turbo-0613": {"max_input_tokens": 4096, "max_output_tokens": 4096},
+    "gpt-3.5-turbo-16k-0613": {"max_input_tokens": 16385, "max_output_tokens": 4096},
+
+    ## gemini models
+    "gemini-1.5-pro": {"max_input_tokens": 1048576, "max_output_tokens": 8192},
+    "gemini-1.5-flash": {"max_input_tokens": 1048576, "max_output_tokens": 8192},
+    "gemini-1.0-pro-001": {"max_input_tokens": 12288, "max_output_tokens": 4096},
+    "gemini-1.0-pro-vision-001": {"max_input_tokens": 12288, "max_output_tokens": 4096},
+
+    ## anthropic models
+    "claude-3-opus-20240229": {"max_input_tokens": 200000, "max_output_tokens": 4096},
+    "claude-3-sonnet-20240229": {"max_input_tokens": 200000, "max_output_tokens": 4096},
+    "claude-3-haiku-20240307": {"max_input_tokens": 200000, "max_output_tokens": 4096}
+
+}
