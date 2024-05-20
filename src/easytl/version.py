fb5c8465d09758a6e030c5b46714c81cd99e62f7
Bikatr7 <Tetralon07@gmail.com>
2024-05-20 00:22:01 -0600

    push second alpha

--- src/easytl/version.py
+++ src/easytl/version.py
@@ -2,4 +2,4 @@
 ## Use of this source code is governed by an GNU Lesser General Public License v2.1
 ## license that can be found in the LICENSE file.
 
-VERSION = "v0.4.0-alpha"
+VERSION = "v0.4.0-alpha-2"
