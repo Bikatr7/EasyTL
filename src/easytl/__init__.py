8d798c3cc45a6c44b8897788349ec3d73af6ebf7
Bikatr7 <Bikatr7@proton.me>
2024-06-22 13:55:17 -0600

    updated copyright headers

--- src/easytl/__init__.py
+++ src/easytl/__init__.py
@@ -1,10 +1,11 @@
-## Copyright (C) 2024 Kaden Bilyeu (Bikatr7) (https://github.com/Bikatr7), Alejandro Mata (https://github.com/alemalvarez)
+## Copyright 2024 Kaden Bilyeu (Bikatr7) (https://github.com/Bikatr7), Alejandro Mata (https://github.com/alemalvarez)
+## EasyTL (https://github.com/Bikatr7/EasyTL)
 ## Use of this source code is governed by an GNU Lesser General Public License v2.1
 ## license that can be found in the LICENSE file.
 
 from .version import VERSION as __version__  # noqa
 
-__author__ = "Kaden Bilyeu (Bikatr7) <Bikatr7@proton.me>"
+__author__ = "Kaden Bilyeu (Bikatr7) <Bikatr7@proton.me>, Alejandro Mata <alemalvarez@icloud.com"
 
 from .easytl import EasyTL
 
--- src/easytl/version.py
+++ src/easytl/version.py
@@ -1,4 +1,5 @@
-## Copyright (C) 2024 Kaden Bilyeu (Bikatr7) (https://github.com/Bikatr7), Alejandro Mata (https://github.com/alemalvarez)
+## Copyright 2024 Kaden Bilyeu (Bikatr7) (https://github.com/Bikatr7), Alejandro Mata (https://github.com/alemalvarez)
+## EasyTL (https://github.com/Bikatr7/EasyTL)
 ## Use of this source code is governed by an GNU Lesser General Public License v2.1
 ## license that can be found in the LICENSE file.
 
